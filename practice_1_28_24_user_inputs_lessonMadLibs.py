5deb6c759888e3d1741f5fc78cf56fdab07541de
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2024-01-28 20:51:40 -0500

    Add files via upload
    fun madLibs game to practice python more to follow. 1 per day for a year or 2.

---
+++ practice_1_28_24_user_inputs_lessonMadLibs.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 23:13:19 2023
+@author: segal
+"""
+#practicing python again from Bro Code
+# at 5 pm to 10 pm daily starting  
+# clip 1 out of 106
+# next madLibs game Practice0
+# first what is madLibs. Well, a story that
+# a user must submit nouns or adjects to make it make sense
+adjective1 = input("Enter an adjective: ") # hard is answer
+noun = input("Enter a noun: ") # woman is answer
+adjective2 = input("Enter an adjective: ") # attentive 
+verb = input("Enter a verb: ") # driving is the answer for the game
+adjective3 = input("Enter an adjective: ") # upset is answer.
+
+print(f"Today I was hit {adjective1} while driving.") # be more creative
+print(f"After work, by a {noun}")
+print(f"The {noun} was not {adjective2} while {verb}.")
+print(f"I was {adjective3} after the hit.")
